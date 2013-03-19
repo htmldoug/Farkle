d80933d247b0a0133914ad0a75f21dea82c848a9
Doug Roper <roperdj@gmail.com>
2013-03-18 23:15:25 -0400

    uncluttered the driver

--- Farkle/src/main/java/org/thedoug/farkle/FarkleDriver.java
+++ Farkle/src/main/java/org/thedoug/farkle/FarkleDriver.java
@@ -1,61 +1,37 @@
 package org.thedoug.farkle;
 
-import org.thedoug.farkle.doug.DougPlayer;
 import org.thedoug.farkle.model.FarkleResult;
 import org.thedoug.farkle.model.LukeRulesScorer;
 import org.thedoug.farkle.model.RandomRoller;
-import org.thedoug.farkle.player.InstrumentedPlayer;
-import org.thedoug.farkle.player.Player;
-import org.thedoug.farkle.player.RollIfAtLeastNRemainingDicePlayer;
-
-import java.util.LinkedHashMap;
-import java.util.Map;
+import org.thedoug.farkle.player.*;
 
 public class FarkleDriver {
+
+    public static final int NUM_MATCHES = 10000;
+
     public static void main(String[] args) {
         RandomRoller rollStrategy = new RandomRoller();
         LukeRulesScorer scorer = new LukeRulesScorer();
 
-        Player[] players = new Player[]{
-                InstrumentedPlayer.instrument(new DougPlayer()),
-                InstrumentedPlayer.instrument(new RollIfAtLeastNRemainingDicePlayer(3)), // Champion!
-        };
+        Player[] players = InstrumentedPlayer.instrumentAll(new Player[]{
+                new RollIfAtLeastNRemainingDicePlayer(2),
+                new RollIfAtLeastNRemainingDicePlayer(3), // Champion!
+                new RollIfAtLeastNRemainingDicePlayer(4),
+                new ConservativePlayer(),
+                new RollAgainOncePlayer(),
+        });
 
-        Map<Player, Integer> scores = new LinkedHashMap<Player, Integer>();
-        for (Player player : players) {
-            scores.put(player, 0);
-        }
+        AggregatedScores totalScores = new AggregatedScores(players);
 
-        Reporter reporter = new Reporter();
-        for (int i = 0; i < 10; i++) {
+        PeriodicProgressReporter progressReporter = new PeriodicProgressReporter();
+        for (int i = 0; i < NUM_MATCHES; i++) {
             FarkleEngine gameEngine = new FarkleEngine(rollStrategy, scorer, players);
-            FarkleResult result = gameEngine.run();
+            FarkleResult singleMatchResult = gameEngine.run();
+            totalScores.add(singleMatchResult);
 
-            Player winner = result.getWinner();
-            scores.put(winner, scores.get(winner) + 1);
-
-            reporter.report(i);
+            progressReporter.logIteration(i);
         }
 
-        for (Map.Entry<Player,Integer> score: scores.entrySet()) {
-            System.out.println(score.getValue() + ": " + score.getKey());
-        }
-    }
-
-    static class Reporter {
-
-        long lastReport = System.currentTimeMillis();
-        int lastIteration = 0;
-
-        public void report(int i) {
-            long now = System.currentTimeMillis();
-            long elapsed = now - lastReport;
-            if (elapsed > 5000) {
-                lastReport = now;
-                long iterationsPerSecond = (i - lastIteration) * 1000 / elapsed;
-                lastIteration = i;
-                System.out.println(i + " matches (" + iterationsPerSecond + "/sec)");
-            }
-        }
+        totalScores.printReport();
     }
 }
