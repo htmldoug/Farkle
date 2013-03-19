7a71b2ed947ab666ef35cbce97ce868e39cab913
Doug Roper <roperdj@gmail.com>
2013-03-18 23:15:03 -0400

    fixed visibility

--- Core/src/main/java/org/thedoug/farkle/player/AbstractPlayer.java
+++ Core/src/main/java/org/thedoug/farkle/player/AbstractPlayer.java
@@ -1,6 +1,6 @@
 package org.thedoug.farkle.player;
 
-abstract class AbstractPlayer implements Player {
+public abstract class AbstractPlayer implements Player {
     @Override
     public String toString() {
         return getClass().getSimpleName();
