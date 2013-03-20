2587145448de2208370a8a87d0a4716348b53519
Doug Roper <roperdj@gmail.com>
2013-03-20 02:54:10 -0400

    fixed reporting of ties

--- Driver/src/main/java/org/thedoug/farkle/AggregatedScores.java
+++ Driver/src/main/java/org/thedoug/farkle/AggregatedScores.java
@@ -4,13 +4,15 @@ import org.thedoug.farkle.model.FarkleResult;
 import org.thedoug.farkle.player.Player;
 
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
  * Tallied scores from multiple Farkle matches.
  */
 public class AggregatedScores {
-    Map<Player, Integer> scores;
+    // TODO switch Map to identity in case somebody implements equals() {return true}
+    private Map<Player, Integer> scores;
 
     public AggregatedScores(Player[] players) {
         scores = initialScores(players);
@@ -25,14 +27,15 @@ public class AggregatedScores {
     }
 
     public void add(FarkleResult result) {
-        Player winner = result.getWinner();
-        scores.put(winner, scores.get(winner) + 1);
+        List<Player> winners = result.getWinners();
+        for (Player winner : winners) {
+            scores.put(winner, scores.get(winner) + 1);
+        }
     }
 
     public void printReport() {
         for (Map.Entry<Player, Integer> score : scores.entrySet()) {
-            System.out.println(score.getValue() + ": " + score.getKey());
+            System.out.println(score.getValue() + " wins : " + score.getKey());
         }
     }
-
 }
