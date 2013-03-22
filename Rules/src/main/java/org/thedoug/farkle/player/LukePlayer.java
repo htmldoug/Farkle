9bda893a671fb7abcc9b8b0303e124b2bb68e7fa
Doug Roper <roperdj@gmail.com>
2013-03-22 17:08:08 -0400

    Gave LukePlayer a better toString.

--- Rules/src/main/java/org/thedoug/farkle/player/LukePlayer.java
+++ Rules/src/main/java/org/thedoug/farkle/player/LukePlayer.java
@@ -3,7 +3,7 @@ package org.thedoug.farkle.player;
 import org.thedoug.farkle.model.GameContext;
 import org.thedoug.farkle.model.Turn;
 
-public class LukePlayer implements Player {
+public class LukePlayer extends AbstractPlayer {
     @Override
     public boolean shouldRollAgain(GameContext gameContext, Turn turn) {
         return false;  // TODO implement me!
